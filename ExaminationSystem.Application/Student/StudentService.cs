d0838602b2e5b0c37bc38638c446eed395bbddcd
yanzhangshuai <yanzhangshuai@126.com>
2025-04-10 22:25:06 +0800

    refactor:整理代码

--- ExaminationSystem.Application/Student/StudentService.cs
+++ ExaminationSystem.Application/Student/StudentService.cs
@@ -4,33 +4,31 @@ namespace ExaminationSystem.Application.Student;
 
 public class StudentService(IUnitOfWork unitOfWork) : IStudentService
 {
-
     public async Task<IEnumerable<string>> Generate()
     {
+        var result = new List<string>();
+
         var repository = unitOfWork.Repository<Model.Student>();
-        
-        // 删除所有数据
-        var ids = await repository.SearchAsync(repository.MultipleResultQuery().Select(s => s.Id));
+
+        var query = repository.MultipleResultQuery().Select(s => s.Id);
+        var ids = await repository.SearchAsync(query);
         if (ids != null && ids.Any())
         {
             foreach (var id in ids)
             {
                 repository.Remove(x => x.Id == id);
             }
+
             await unitOfWork.SaveChangesAsync();
         }
-        
-        var result = new List<string>();
-        
-        var list = new List<Model.Student>();
 
-        var random = new Random();
-        var count = random.Next(20, 50);
+        var list = new List<Model.Student>();
+        var count = new Random().Next(20, 50);
         for (var i = 0; i < count; i++)
         {
             var name = "考生" + (i + 1);
-            result.Add(name);
             list.Add(new Model.Student { Name = name });
+            result.Add(name);
         }
 
         await repository.AddRangeAsync(list);
@@ -41,25 +39,26 @@ public class StudentService(IUnitOfWork unitOfWork) : IStudentService
 
     public async Task<IEnumerable<string>> List()
     {
-        var repository = unitOfWork.Repository<Model.Student>();
-        var query = repository.MultipleResultQuery().OrderBy(x => x.Id).Select(x => x.Name);
-        var names = await repository.SearchAsync(query);
-
         var result = new List<string>();
-        
-        int left = 0, 
-            right = names.Count - 1;
+
+        var repository = unitOfWork.Repository<Model.Student>();
+
+        var query = repository.MultipleResultQuery().OrderBy(x => x.Id).Select(x => x.Name);
+        var list = await repository.SearchAsync(query);
+
+        int left = 0,
+            right = list.Count - 1;
 
         while (left <= right)
         {
             if (left == right)
             {
-                result.Add(names[left]);
+                result.Add(list[left]);
             }
             else
             {
-                result.Add(names[left]);
-                result.Add(names[right]);
+                result.Add(list[left]);
+                result.Add(list[right]);
             }
 
             left++;
