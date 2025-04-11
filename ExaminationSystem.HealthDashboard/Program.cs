449c95a1124e88022e692fd2114aa10b0782c443
yanzhangshuai <yanzhangshuai@126.com>
2025-04-11 17:33:05 +0800

    fix:整理日志级别

--- ExaminationSystem.HealthDashboard/Program.cs
+++ ExaminationSystem.HealthDashboard/Program.cs
@@ -18,10 +18,12 @@ builder.Services.AddHealthChecksUI(options =>
     var endpoints = builder.Configuration.GetSection("HealthCheckEndpoints");
     foreach (var endpoint in endpoints.GetChildren())
     {
-        if (endpoint.Value != null)
-            options.AddHealthCheckEndpoint(endpoint.Key, endpoint.Value)
-                .SetEvaluationTimeInSeconds(30) // 检查间隔
-                .SetMinimumSecondsBetweenFailureNotifications(60); // 失败通知间隔
+        if (endpoint.Value == null) continue;
+        
+        var key = $"{endpoint.Key}(${endpoint.Value})";
+        options.AddHealthCheckEndpoint(key, endpoint.Value)
+            .SetEvaluationTimeInSeconds(30) // 检查间隔
+            .SetMinimumSecondsBetweenFailureNotifications(60); // 失败通知间隔
     }
 
 
